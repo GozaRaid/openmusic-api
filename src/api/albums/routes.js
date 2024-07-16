32ee24dd9e4b90477fd95b0a80e52bd208e01421
GozaRaid <hafidzhitobaihaqi@gmail.com>
2024-07-17 01:18:27 +0700

    initial commit

---
+++ src/api/albums/routes.js
@@ -0,0 +1,29 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: handler.postAlbumHandler,
+  },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: handler.getAlbumByIdHandler,
+  },
+  // {
+  //   method: 'GET',
+  //   path: '/albums/{albumId}',
+  //   handler: handler.getAlbumByAlbumIdHandler,
+  // },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: handler.putAlbumByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: handler.deleteAlbumByIdHandler,
+  },
+];
+
+module.exports = routes;
